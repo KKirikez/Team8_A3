fdf8f5f72f3c36d2dc2e8ca97f8399afb86dfe99
ellicrw <150177087+ellicrw@users.noreply.github.com>
2024-03-27 16:50:33 -0600

    Added our old stuff (unfixed)

---
+++ A2Code/src/application/AppDriver.java
@@ -0,0 +1,14 @@
+package application;
+
+import controller.Coordinator;
+
+/**
+ * The main class that drives the application.
+ */
+public class AppDriver {
+	public static void main(String[] args) {
+		//Main file
+		Coordinator.mainMenu();
+		
+	}
+}
