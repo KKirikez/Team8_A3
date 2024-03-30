e0474871db7db4a6a72778a52f91b1bd3f7eae6d
Kostya Kirikeza <36024647+KKirikez@users.noreply.github.com>
2024-03-29 20:22:24 -0600

    added home menu design

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,54 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
 public class SampleController {
-	
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private TextField nameImput;
+
+    @FXML
+    private RadioButton nameSort;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TextField serialNumInput;
+
+    @FXML
+    private RadioButton serialNumSort;
+
+    @FXML
+    private ToggleGroup sortButtons;
+
+    @FXML
+    private TextField typeInput;
+
+    @FXML
+    private RadioButton typeSort;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sortButtonPressed(ActionEvent event) {
+
+    }
+
 }
